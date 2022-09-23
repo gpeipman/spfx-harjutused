f0487a1d36e8d239d6f7e0b3121a2e96ddea3d77
Gunnar <gpeipman@hotmail.com>
2022-09-23 16:09:59 +0300

    13, 14 - excel task pane

---
+++ 14-ExcelTaskPane2/My Office Add-in/src/dialogs/popup.js
@@ -0,0 +1,14 @@
+(function () {
+    "use strict";
+    
+        Office.onReady()
+            .then(function() {    
+                document.getElementById("ok-button").onclick = sendStringToParentPage;
+            });
+    
+        function sendStringToParentPage() {
+            const userName = document.getElementById("name-box").value;
+            Office.context.ui.messageParent(userName);
+        }
+    
+    }());
